d17aae72b2aa386ce3000e8ea4a682b541774603
Katsuki Chan <katsukidotxyz@gmail.com>
2023-03-13 16:00:28 -0400

    feat: specify typography vars (#4)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontSize: {
+      sm: ['16px', '21px'],
+      md: ['20px', '26px'],
+      lg: ['24px', 'auto'],
+      xl: ['32px', 'auto'],
+      '2xl': ['40px', 'auto'],
+      '3xl': ['56px', 'auto'],
+      '4xl': ['64px', 'auto'],
+    },
+    fontWeight: {
+      thin: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      black: '900',
+    },
     extend: {
       colors: {
         light: {
